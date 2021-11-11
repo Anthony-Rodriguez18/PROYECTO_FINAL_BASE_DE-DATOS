55143c882cb93186d95f37ac9b7fc4c968e61a70
Aria315 <85749986+Aria315@users.noreply.github.com>
2021-11-11 12:16:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,3 +4,18 @@ INTEGRANTES:
 - Josue Carpio 
 - Ariana Leon 
 - Anthony Rodriguez */
+
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+void error()
+{
+  cout<<"No se pudo abrir el archivo de registros, asegurese que el archivo se encuentre en\n";
+  cout<<"la misma ubicación que el programa o que el archivo de texto se llame: \n";
+  cout<<"usuarios.txt, si el archivo tiene otro nombre renómbrelo al ya mencionado\n\n";
+}
+
